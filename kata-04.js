ff3565d26161f37fca3f3092055b9f6aec4c01c7
Nguyễn Thanh Sơn <son.thanh.97@gmail.com>
2019-06-10 17:07:38 +0700

    Complete kata 03, 04, 41, 42, 43

---
+++ kata-04.js
@@ -0,0 +1,34 @@
+// 4: template strings - String.raw
+// To do: make all tests pass, leave the asserts unchanged!
+// Follow the hints of the failure messages!
+
+describe('Use the `raw` property of tagged template strings like so `s.raw`', function() {
+  it('the `raw` property accesses the string as it was entered', function() {
+    function firstChar(strings) {
+      return strings.raw[0];
+    }
+    assert.equal(firstChar`\n`, '\\n');
+  });
+  it('`raw` can access the backslash of a line-break', function() {
+    function firstCharEntered(strings) {
+      var lineBreak = strings.raw[0];
+      return lineBreak[0];
+    }
+    assert.equal(firstCharEntered`\n`, '\\');
+  });
+  describe('`String.raw` as a static function', function(){
+    it('concats the raw strings', function() {
+      var expected = '\\n';
+      assert.equal(String.raw`\n`, expected);
+    });
+    it('two raw-templates-string-backslashes equal two escaped backslashes', function() {
+      const TWO_BACKSLASHES = '\\\\';
+      assert.equal(String.raw`\\`, TWO_BACKSLASHES);
+    });
+    it('works on unicodes too', function() {
+      var smilie = '\\u{1F600}';
+      var actual = String.raw`\u{1F600}`;
+      assert.equal(actual, smilie);
+    });
+  });
+});
