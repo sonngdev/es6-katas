6bf406a2f8601034c15e2776a9bdc7d7c371477c
Nguyễn Thanh Sơn <son.thanh.97@gmail.com>
2019-06-14 11:59:02 +0700

    Complete kata 23, 24, 25

---
+++ kata-25.js
@@ -0,0 +1,36 @@
+// 25: class - extends
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('Classes can inherit from another using `extends`', () => {
+  describe('the default super class is `Object`', () => {
+    it('a `class A` is an instance of `Object`', () => {
+      class A {}
+      assert.equal(new A() instanceof Object, true);
+    });
+    it('when B extends A, B is also instance of `Object`', () => {
+      class A {}
+      class B extends A {}
+      assert.equal(new B() instanceof A, true);
+      assert.equal(new B() instanceof Object, true);
+    });
+    it('a class can extend `null`, and is not an instance of Object', () => {
+      class NullClass extends null {}
+      let nullInstance = new NullClass();
+      assert.equal(nullInstance instanceof Object, false);
+    });
+  });
+  describe('instance of', () => {
+    it('when B inherits from A, `new B()` is also an instance of A', () => {
+      class A {}
+      class B extends A {}
+      assert.equal(new B() instanceof A, true);
+    });
+    it('extend over multiple levels', () => {
+      class A {}
+      class B extends A {}
+      class C extends B {}
+      assert.equal(new C() instanceof A, true);
+    });
+  });
+});
